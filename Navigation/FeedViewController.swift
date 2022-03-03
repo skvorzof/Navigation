83424ee2673f345d1e2e7e174ad0232cc2031de6
Dima Skvortsov <4594651+skvorzof@users.noreply.github.com>
2022-03-03 16:43:53 +0300

    Добавил TabBarController

---
+++ Navigation/FeedViewController.swift
@@ -0,0 +1,41 @@
+//
+//  FeedViewController.swift
+//  Navigation
+//
+//  Created by mitr on 03.03.2022.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    struct Post {
+        let title: String
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 30))
+        button.center = view.center
+        button.backgroundColor = .systemBlue
+        button.setTitle("Смотреть запись", for: .normal)
+        button.addTarget(self, action: #selector(tap), for: .touchUpInside)
+        
+        view.addSubview(button)
+    }
+    
+    @objc func tap(sender: UIButton) {
+        let post = Post(title: "Запись")
+        let postVC = PostViewController()
+        postVC.title = post.title
+        postVC.navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "info.circle"),
+            style: .done,
+            target: self,
+            action: nil)
+        
+        navigationController?.pushViewController(postVC, animated: true)
+    }
+}
---
+++ Navigation/TabBarController.swift
@@ -0,0 +1,39 @@
+//
+//  TabBarController.swift
+//  Navigation
+//
+//  Created by mitr on 03.03.2022.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        UITabBar.appearance().barTintColor = .systemBackground
+        tabBar.tintColor = .label
+        createTabBarControllers()
+    }
+    
+    func createTabBarControllers() {
+        viewControllers = [
+            createNavController(for: FeedViewController(),
+                                   title: "Лента", image: UIImage(systemName: "list.dash.header.rectangle")!),
+            createNavController(for: ProfileViewController(),
+                                   title: "Профиль",
+                                   image: UIImage(systemName: "person")!)
+        ]
+    }
+    
+    func createNavController(for rootViewController: UIViewController,
+                             title: String,
+                             image: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        rootViewController.navigationItem.title = title
+        return navController
+    }
+}
