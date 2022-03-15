f15ee6c42d21e09ac69209bd38d7b53512a4a892
Dima Skvortsov <4594651+skvorzof@users.noreply.github.com>
2022-03-15 12:35:14 +0300

    ДЗ 2.2

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -14,12 +14,12 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 209.0/255.0, green: 209.0/255.0, blue: 214.0/255.0, alpha: 1.0)
+        layout()
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        profileHeaderView.frame = self.view.frame
-        view.addSubview(profileHeaderView)
+        profileHeaderView.frame = self.view.bounds
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -27,5 +27,36 @@ class ProfileViewController: UIViewController {
             let appearance = UINavigationBarAppearance()
             appearance.backgroundColor = .white
             navigationController?.navigationBar.scrollEdgeAppearance = appearance
+    }
+    
+    
+    private let setTitleButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Изменить заголовок", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 14)
+        button.backgroundColor = .systemBlue
+        return button
+    }()
+    
+    
+    private func layout() {
+        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(profileHeaderView)
+        view.addSubview(setTitleButton)
+        
+        NSLayoutConstraint.activate([
+            profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            profileHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            profileHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            profileHeaderView.heightAnchor.constraint(equalToConstant: 220)
+        ])
+        
+        NSLayoutConstraint.activate([
+            setTitleButton.heightAnchor.constraint(equalToConstant: 50),
+            setTitleButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            setTitleButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            setTitleButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
